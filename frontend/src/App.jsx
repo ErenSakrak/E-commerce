4ff233e3fd327eb5f67ffded9f9c94b97220997c
ErenSakrak <eren.sakrak1453@gmail.com>
2024-03-08 15:40:19 +0300

    new E-commerce Project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Layout/Header/Header";
+import Footer from "./components/Layout/Footer/Footer";
+import Policy from "./components/Layout/Policy/Policy";
+import Sliders from "./components/Slider/Sliders";
+function App() {
+  return (
+    <>
+      <Header />
+      <Sliders />
+      <Policy />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
